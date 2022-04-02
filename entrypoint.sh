d58264ab36508c2e9a8253a663700e9cef1adacc
Wietse de Vries <wietsedvries@gmail.com>
2022-04-02 09:33:55 +0200

    use s6 overlay

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-contenv bash
+
+umask ${UMASK:-022}
 
 calibre --version
 
-[ ! -f "/config/users.sqlite" ] && calibre-server --userdb /config/users.sqlite --manage-users -- add "${USERNAME:-admin}" "${USERNAME:-admin}"
-[ ! -f "/books/metadata.db" ] && touch /books/metadata.db
+[ ! -f "/config/users.sqlite" ] && s6-setuidgid abc calibre-server --userdb /config/users.sqlite --manage-users -- add "${USERNAME:-admin}" "${USERNAME:-admin}"
+[ ! -f "/books/metadata.db" ] && s6-setuidgid abc touch /books/metadata.db
 
-XDG_RUNTIME_DIR=/tmp/runtime-root calibre-server \
+XDG_RUNTIME_DIR=/tmp/runtime-root s6-setuidgid abc calibre-server \
     --disable-use-bonjour \
     --enable-local-write \
     --userdb=/config/users.sqlite \
